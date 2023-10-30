77c2bdf2e66204ce43f6f7f56272a50f94f77ca2
Attila Horvath <sweeptic@gmail.com>
2023-10-30 12:41:21 +0100

    Intercept input property changes with ngOnChanges

--- src/app/component-communication/version-parent/version-parent.component.ts
+++ src/app/component-communication/version-parent/version-parent.component.ts
@@ -3,9 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-version-parent',
   templateUrl: './version-parent.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VersionParentComponent {
+  major = 1;
+  minor = 23;
 
+  newMinor() {
+    this.minor++;
+  }
+
+  newMajor() {
+    this.major++;
+    this.minor = 0;
+  }
 }
