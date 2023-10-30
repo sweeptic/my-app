370cad06d110d63ccb70b5e1c4c9969c9f2a4777
Attila Horvath <sweeptic@gmail.com>
2023-10-30 13:55:20 +0100

    Parent and children communicate via a service

--- src/app/component-communication/astronaut/astronaut.component.ts
+++ src/app/component-communication/astronaut/astronaut.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MissionService } from '../mission.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-astronaut',
   templateUrl: './astronaut.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AstronautComponent {
+  @Input() astronaut = '';
+  mission = '<no mission announced>';
+  confirmed = false;
+  announced = false;
+  subscription: Subscription;
 
+  constructor(private missionService: MissionService) {
+    this.subscription = missionService.missionAnnounced$.subscribe(
+      (mission) => {
+        this.mission = mission;
+        this.announced = true;
+        this.confirmed = false;
+      }
+    );
+  }
+
+  confirm() {
+    this.confirmed = true;
+    this.missionService.confirmMission(this.astronaut);
+  }
+
+  ngOnDestroy() {
+    // prevent memory leak when component destroyed
+    this.subscription.unsubscribe();
+  }
 }
