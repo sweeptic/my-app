963b81b1af1aec6e42a4b9336ceb3074abd90dd2
Attila Horvath <sweeptic@gmail.com>
2023-10-30 12:26:01 +0100

    Intercept input property changes with a setter

--- src/app/component-communication/name-child/name-child.component.ts
+++ src/app/component-communication/name-child/name-child.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-name-child',
   templateUrl: './name-child.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NameChildComponent {
+  private _name = '';
 
+  @Input()
+  get name(): string {
+    return this._name;
+  }
+  set name(name: string) {
+    this._name = (name && name.trim()) || '<no name set>';
+  }
 }
--- src/app/component-communication/name-parent/name-parent.component.ts
+++ src/app/component-communication/name-parent/name-parent.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-name-parent',
   templateUrl: './name-parent.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NameParentComponent {
-
+  names = ['Dr. IQ', '   ', '  Bombasto  '];
 }
