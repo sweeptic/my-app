5d64f32059c7849fcba8c7a268cee63ebfc75609
Attila Horvath <sweeptic@gmail.com>
2023-10-30 12:53:53 +0100

    Parent listens for child event

--- src/app/component-communication/voter/voter.component.ts
+++ src/app/component-communication/voter/voter.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-voter',
   templateUrl: './voter.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VoterComponent {
+  didVote = false;
+  @Input() name = '';
+  @Output() voted = new EventEmitter<boolean>();
 
+  vote(agreed: boolean) {
+    this.voted.emit(agreed);
+    this.didVote = true;
+  }
 }
--- src/app/component-communication/votetaker/votetaker.component.ts
+++ src/app/component-communication/votetaker/votetaker.component.ts
@@ -3,9 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-votetaker',
   templateUrl: './votetaker.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VotetakerComponent {
+  agreed = 0;
+  disagreed = 0;
+  voters = ['Dr. IQ', 'Celeritas', 'Bombasto'];
 
+  onVoted(agreed: boolean) {
+    if (agreed) {
+      this.agreed++;
+    } else {
+      this.disagreed++;
+    }
+  }
 }
