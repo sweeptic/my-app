d99646a4414dc7da8f70a32ac0928d1374fbdd5a
Attila Horvath <sweeptic@gmail.com>
2023-11-04 22:05:32 +0100

    fix standalone

--- src/app/standalone-components/standalone-component/standalone-component.component.ts
+++ src/app/standalone-components/standalone-component/standalone-component.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { ImageGridComponent } from '../image-grid/image-grid.component';
-import { AppModule } from 'src/app/app.module';
+
 
 @Component({
     standalone: true,
     selector: 'app-standalone-component',
     templateUrl: './standalone-component.component.html',
-    imports: [ImageGridComponent, AppModule]
+    imports: [ImageGridComponent,]
 })
 export class StandaloneComponentComponent {
 
