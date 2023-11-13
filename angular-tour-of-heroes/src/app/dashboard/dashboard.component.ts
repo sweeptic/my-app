fc7379f01b2d2fd1cd60b992888aa109aace5d0d
Attila Horvath <sweeptic@gmail.com>
2023-11-13 18:31:19 +0100

    list of dashboard

--- angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Hero } from '../services/in-memory-data.service';
+import { HeroService } from '../services/hero.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+    heroes: Hero[] | undefined;
+
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+
+    getHeroes() {
+        this.heroService.getHeroes().subscribe((data) => {
+            console.log('data', data);
+            this.heroes = data.slice(0, 4);
+        });
+    }
 
 }
