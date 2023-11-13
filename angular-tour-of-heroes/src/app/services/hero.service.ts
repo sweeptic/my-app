b593d250cf1a5bbcd90d7fc147f60baa31cebb42
Attila Horvath <sweeptic@gmail.com>
2023-11-13 20:38:11 +0100

    add error handling

--- angular-tour-of-heroes/src/app/services/hero.service.ts
+++ angular-tour-of-heroes/src/app/services/hero.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable, of, tap } from 'rxjs';
+import { Observable, catchError, of, tap } from 'rxjs';
 import { Hero } from './in-memory-data.service';
 import { MessageService } from './message.service';
 
@@ -24,24 +24,25 @@ export class HeroService {
 
     getHeroes(): Observable<Hero[]> {
         return this.http.get<Hero[]>(this.heroesUrl).pipe(
-            tap(_ => {
-                this.messageService.addMessage("fetch hero");
-            })
+            tap(_ => this.messageService.addMessage("fetch hero")),
+            catchError(this.handleError<any>('getHeroes', []))
         );
     }
 
     getHero(id: number) {
         const url = `${this.heroesUrl}/${id}`;
         return this.http.get<Hero>(url).pipe(
-            tap((hero) => this.messageService.addMessage(`HeroService: Get hero ${hero}`)
-            ));
+            tap((hero) => this.messageService.addMessage(`HeroService: Get hero ${hero}`)),
+            catchError(this.handleError<any>('getHero'))
+        );
     }
 
     addHero(hero: Hero): Observable<Hero> {
         return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
             .pipe(
                 tap((hero) => {
-                    this.messageService.addMessage(`HeroService: Add hero ${hero}`);
+                    this.messageService.addMessage(`HeroService: Add hero ${hero}`),
+                        catchError(this.handleError<any>('updateHero'));
                 })
             );
     }
@@ -50,8 +51,9 @@ export class HeroService {
     updateHero(hero: Hero) {
         return this.http.put(this.heroesUrl, hero, this.httpOptions)
             .pipe(
-                tap(_ => this.messageService.addMessage(`HeroService: Update hero ${hero}`)
-                ));
+                tap(_ => this.messageService.addMessage(`HeroService: Update hero ${hero}`)),
+                catchError(this.handleError<any>('updateHero'))
+            );
     }
 
     searchHeroes(term: string) {
@@ -59,13 +61,45 @@ export class HeroService {
             return of([]);
         }
         return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
-            tap(_ => this.messageService.addMessage(`HeroService: Search hero term ${term}`)));
+            tap(_ => this.messageService.addMessage(`HeroService: Search hero term ${term}`)),
+            catchError(this.handleError<any>('searchHeroes'))
+        );
     }
 
     removeHero(id: number) {
         const url = `${this.heroesUrl}/${id}`;
         return this.http.delete<Hero>(url, this.httpOptions).pipe(
-            tap(_ => this.messageService.addMessage(`HeroService: Deleted hero id ${id}`)));
+            tap(_ => this.messageService.addMessage(`HeroService: Deleted hero id ${id}`)),
+            catchError(this.handleError<any>('removeHero'))
+        );
     }
+
+
+    /**
+       * Handle Http operation that failed.
+       * Let the app continue.
+       *
+       * @param operation - name of the operation that failed
+       * @param result - optional value to return as the observable result
+       */
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
+
+            // TODO: better job of transforming error for user consumption
+            this.log(`${operation} failed: ${error.message}`);
+
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
+    }
+
+    /** Log a HeroService message with the MessageService */
+    private log(message: string) {
+        this.messageService.addMessage(`HeroService: ${message}`);
+    }
+
 
 }
